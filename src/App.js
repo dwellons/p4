d4faf120aef00fc8b87031d9a84cbb4541c64b95
Darin <dwellons@madisoncollege.edu>
2024-11-13 17:16:17 -0600

    uploaded the buildspec.yml file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1>My First React App - Hello World!</h1>
+            <h1>My First React App - Hello Wisconsin! - From Darin Wellons</h1>
         </div>
     );
 }
